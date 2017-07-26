cb81b50f31f222b1af7ecd5f0de67926ce4d4c78
Bikee Maharjan <bikeemaharjan67@gmail.com>
2017-07-27 01:42:02 +0545

    first test

--- app.js
+++ app.js
@@ -14,7 +14,25 @@ console.log(typeof tools.foo);
 
 tools.convertToJson();
 
-//tools.convertToXml();
+//tools.convertToXml('/quote.xml');
+
+//tools.convertToHtml('/quote.html');
 
 console.log('Server running at http://127.0.0.1:'+port);
 
+/*var http=require('http');
+var url=require('url');
+
+var server=http.createServer(function(req,res){
+    var pathname=url.parse(req.url).pathname;
+    switch(pathname){
+        case '/subpage':
+            res.end('subpage');
+        break;
+        default:
+            res.end('default');
+        break;
+    }
+
+}).listen(8080);
+*/
